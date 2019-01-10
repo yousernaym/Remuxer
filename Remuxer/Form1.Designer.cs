7a51cc81fc5ebd6ca0b10b04600a125393a8ac19
kloda <mickeh.se@hotmail.com>
2019-01-10 13:53:20 +0100

    Added libs, changed names and made some minor fixes.

---
+++ Remuxer/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Remux
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.progressLabel = new System.Windows.Forms.Label();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.SuspendLayout();
+			// 
+			// progressLabel
+			// 
+			this.progressLabel.AutoSize = true;
+			this.progressLabel.Location = new System.Drawing.Point(12, 38);
+			this.progressLabel.Name = "progressLabel";
+			this.progressLabel.Size = new System.Drawing.Size(35, 13);
+			this.progressLabel.TabIndex = 0;
+			this.progressLabel.Text = "label1";
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(12, 12);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(259, 23);
+			this.progressBar1.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(283, 66);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.progressLabel);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label progressLabel;
+		private System.Windows.Forms.ProgressBar progressBar1;
+	}
+}
+
---
+++ Remuxer/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Remux
+{
+	public partial class Form1 : Form
+	{
+		string inputPath = "";
+		string audioPath = "";
+		string midiPath = "";
+		bool incorrectFlags = false;
+		public Form1(string []args) : base()
+		{
+			InitializeComponent();
+			bool midiFlag = false, audioFlag = false;
+
+			if (args == null || args.Length == 0)
+				showErrorMsg("No arguments specified.");
+
+			for (int i = 0; i < args.Length; i++)
+			{
+				string arg = args[i];
+				if (arg[0] == '-')
+				{
+					if (arg == "-m")
+						midiFlag = true;
+					else if (arg == "-a")
+						audioFlag = true;
+					else
+						showErrorMsg($"Invalid flag{arg}");
+				}
+				else
+				{
+					if (midiFlag)
+						midiPath = arg;
+					else if (audioFlag)
+						audioPath = arg;
+					else
+						inputPath = arg;
+
+				}
+			}
+		}
+
+		private void showErrorMsg(string msg)
+		{
+			MessageBox.Show(msg);
+			incorrectFlags = true;
+		}
+		private async void Form1_Load(object sender, EventArgs e)
+		{
+			if (incorrectFlags)
+				Close();
+			await Task.Run( delegate
+			{
+				for (int i = 0; i < 100; i++)
+				{
+					progressBar1.BeginInvoke(new Action(()=> progressBar1.Value += 1));
+					Thread.Sleep(200);
+				}
+			});
+			Close();
+		}
+	}
+}
