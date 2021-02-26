c90ca0564f3be80334bf50a390deec50f68aa40b
Yousernaym <mickeh.se@hotmail.com>
2021-02-26 12:25:36 +0100

    TopMost = true

--- Remuxer/Form1.Designer.cs
+++ Remuxer/Form1.Designer.cs
@@ -59,7 +59,9 @@
 			this.Controls.Add(this.processInfo);
 			this.Controls.Add(this.progressBar1);
 			this.KeyPreview = true;
+			this.MaximizeBox = false;
 			this.Name = "Form1";
+			this.TopMost = true;
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
